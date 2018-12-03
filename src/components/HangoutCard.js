70d2811e516e4ebe218cd8bcdf717ac6d29b176a
Daniel Kukula <daniel.kuku@gmail.com>
2018-12-03 23:58:24 +0000

    Card styling

--- src/components/HangoutCard.js
+++ src/components/HangoutCard.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { fetchHangouts } from "../actions/getHangoutsAction";
-import { Card, Container, Image } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
 import YouTube from "react-youtube";
 
 export default class HangoutCard extends Component {
+  youtubeURL(id) {
+    return `https://www.youtube.com/watch?v=${id}`
+  }
   render(){
     const { hangout, opts } = this.props
     return (
       <Card key={hangout.id} >
         <Image>
-          <YouTube videoId={hangout.yt_video_id} opts={opts}/>
+          <YouTube videoId={hangout.yt_video_id} opts={opts}>
+          </YouTube>
         </Image>
         <Card.Content>
-          <Link to={`/${hangout.id}`}>
-            <Card.Header>{hangout.title}</Card.Header>
-          </Link>
-          <Card.Meta>
-            {hangout.category}
-      </Card.Meta>
-    </Card.Content>
-    <Card.Description>
-    </Card.Description>
+          <Card.Header>{hangout.title}</Card.Header>
+          <Card.Meta> {hangout.category}
+          </Card.Meta>
+        </Card.Content>
+        <Card.Description>
+          <Button.Group floated="right">
+          <a href={hangout.hangout_url}>
+            <Button primary >Join</Button>
+          </a>
+          <a href={this.youtubeURL(hangout.yt_video_id)}>
+            <Button secondary>Watch</Button>
+          </a>
+        </Button.Group>
+        </Card.Description>
       </Card>
     )
   }
